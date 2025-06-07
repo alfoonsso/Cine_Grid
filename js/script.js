c37239a0f4c1af6ab316a946a21645ced02305ae
Alfonso <alfonsojimenezg00@gmail.com>
2025-06-07 10:19:47 +0200

    Init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Configuración de la API de TMDB
-import { API_KEY, BASE_URL, IMAGE_BASE_URL, PERSON_IMAGE_BASE_URL } from '../config/tmbd.js';
+const API_KEY = '98fd1e135ecfac794b25c1fe1d162ac8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const PERSON_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2'; // Usando el formato específico del ejemplo
 
 // Mapeo de directores a sus IDs en TMDB
 const directors = {
