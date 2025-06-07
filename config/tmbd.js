c76dafb81308b72f95156ab32861f4899acad6db
Alfonso <alfonsojimenezg00@gmail.com>
2025-06-07 09:35:07 +0200

    Changes

---
+++ config/tmbd.js
@@ -0,0 +1,4 @@
+const API_KEY = '98fd1e135ecfac794b25c1fe1d162ac8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const PERSON_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2'; // Usando el formato específico del ejemplo
